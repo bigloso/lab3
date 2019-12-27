1dae31c0d2f03a19516fa141a93b35ff8c1d9b14
Bigloso <wayside.big@gmail.com>
2019-12-27 16:21:00 +0700

    num_1

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -2,5 +2,14 @@
 using namespace std ;
 
 int main(){
-    
+    float exp , num=2 ;
+    while(num<=69){
+        exp += 1/num ; 
+        num++ ;
+        
+    }
+
+    cout << exp ;
+
+    return 0 ;
 }
